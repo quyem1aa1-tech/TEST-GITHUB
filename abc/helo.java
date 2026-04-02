3b0ea06dba0f5f937876a8f58b0161d6102d5681
Vy Ha <vievievoq@MacBook-Pro-cua-Vy.local>
2026-04-02 07:35:38 -0700

    abc

---
+++ abc/helo.java
@@ -0,0 +1,18 @@
+
+public class libarary {
+    private int a;
+    private int b;
+
+    public libarary(int a, int b) {
+        this.a = a;
+        this.b = 0;
+    }
+
+    public int getIntA() {
+        return a;
+    }
+
+    public int getIntB() {
+        return b;
+    }
+}
